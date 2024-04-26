fc889551df0621c8a0c1176658443bd23d9f9a23
iftikhrlutfur <iftikherlutfur@gmail.com>
2024-04-27 03:52:49 +0600

    setup the firebase

--- src/Components/firebase.config.js
+++ src/Components/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBdZ-R9S0aO3vlOcsbY-UEcqWRarGzO-kM",
-  authDomain: "art-and-craft-30730.firebaseapp.com",
-  projectId: "art-and-craft-30730",
-  storageBucket: "art-and-craft-30730.appspot.com",
-  messagingSenderId: "180029377194",
-  appId: "1:180029377194:web:2d53bb9cf1539909ca1ed3"
+  apiKey: import.meta.env.API_KEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
