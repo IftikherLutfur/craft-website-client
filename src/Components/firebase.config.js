310d0cf9124a533305fd867e95a691b84cc0c940
iftikhrlutfur <iftikherlutfur@gmail.com>
2024-07-06 16:34:59 +0600

    setup a new firebase

--- src/Components/firebase.config.js
+++ src/Components/firebase.config.js
@@ -1,24 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // apiKey: import.meta.env.API_KEY,
-  // authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  // projectId: import.meta.env.VITE_PROJECTID,
-  // storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  // messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  // appId: import.meta.env.VITE_APPID
-  apiKey: "AIzaSyBdZ-R9S0aO3vlOcsbY-UEcqWRarGzO-kM",
-  authDomain: "art-and-craft-30730.firebaseapp.com",
-  projectId: "art-and-craft-30730",
-  storageBucket: "art-and-craft-30730.appspot.com",
-  messagingSenderId: "180029377194",
-  appId: "1:180029377194:web:2d53bb9cf1539909ca1ed3"
+  apiKey: "AIzaSyBEX2Bh1cr4X-YOQncxxKI2MVZXez4wmEo" ,
+  authDomain: "hotel-booking-325eb.firebaseapp.com" ,
+  projectId: "hotel-booking-325eb" ,
+  storageBucket: "hotel-booking-325eb.appspot.com" ,
+  messagingSenderId: "362405851595" ,
+  appId: "1:362405851595:web:88d2b929ab475fccc4c64",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export default app;
